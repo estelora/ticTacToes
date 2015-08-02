40d827df4b6ffe504f382bf5730407a07c3b9b9f
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-08-01 20:40:52 -0400

    add ng-app and 404 handling

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,10 @@
-// The namespace for this application
-var app = {};
+var game = angular.module('game', ['ngRoute']);
+
+// //Create our 404 handler
+game.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider.otherwise({
+    controller: 'Error404Ctrl',
+    controllerAs: 'vm',
+    templateUrl: '/error-404.html',
+  });
+}]);
